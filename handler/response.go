3c733f3c7a77e9ac5d20a01b60ef831a854753f6
y_uuki <yuki.tsubo@gmail.com>
2016-10-10 00:41:43 +0900

    Add handler util for building response

---
+++ handler/response.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/yuuki/dynamond/log"
+)
+
+func JSON(w http.ResponseWriter, status int, v interface{}) error {
+	res, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+	return nil
+}
+
+func BadRequest(w http.ResponseWriter) {
+	log.Println(msg)
+
+	var data struct {
+		e  string `json:"error"`
+	}
+	data.e = msg
+	JSON(w, http.StatusBadRequest, data)
+	return
+}
+
+func NotFound(w http.ResponseWriter) {
+	http.Error(w, "404 Not Found", http.StatusNotFound)
+}
+
+func ServerError(w http.ResponseWriter, msg string) {
+	log.Println(msg)
+
+	var data struct {
+		e  string `json:"error"`
+	}
+	data.e = msg
+	JSON(w, http.StatusInternalServerError, data)
+	return
+}
+
