0c406dad7cd402ce8c01d1be5f5fc505891c1250
y_uuki <yuki.tsubo@gmail.com>
2016-11-05 01:50:16 +0900

    Add test case for engine.EvalTarget nested func expression

--- query/engine_test.go
+++ query/engine_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/yuuki/dynamond/tsdb"
 )
 
-func TestEvalTarget_Alias(t *testing.T) {
+func TestEvalTarget_Func(t *testing.T) {
 	ctrl := tsdb.SetMockDynamoDB(t, &tsdb.MockDynamoDB{
 		TableName: "SeriesTest",
 		StartVal: time.Unix(1465516800, 0),
@@ -32,5 +32,30 @@ func TestEvalTarget_Alias(t *testing.T) {
 	if assert.NoError(t, err) {
 		assert.Equal(t, 1, len(metricList))
 		assert.Equal(t, metricList[0].Name, "Large Blue Widgets")
+	}
+}
+
+func TestEvalTarget_FuncNest(t *testing.T) {
+	ctrl := tsdb.SetMockDynamoDB(t, &tsdb.MockDynamoDB{
+		TableName: "SeriesTest",
+		StartVal: time.Unix(1465516800, 0),
+		EndVal: time.Unix(1465526800, 0),
+		Metric: model.NewMetric(
+			"Sales.widgets.largeBlue",
+			[]*model.DataPoint{
+				&model.DataPoint{1465516810, 10.0},
+			},
+		),
+	})
+	defer ctrl.Finish()
+
+	metricList, err := EvalTarget(
+		"alias(alias(Sales.widgets.largeBlue,\"Large Blue Widgets\"),\"Large Blue Widgets Sales\")",
+		time.Unix(1465516800, 0),
+		time.Unix(1465526800, 0),
+	)
+	if assert.NoError(t, err) {
+		assert.Equal(t, 1, len(metricList))
+		assert.Equal(t, metricList[0].Name, "Large Blue Widgets Sales")
 	}
 }
