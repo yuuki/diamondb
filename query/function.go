368192719564c2f47ad316711a8af80904fac736
y_uuki <yuki.tsubo@gmail.com>
2016-12-18 00:29:56 +0900

    Add min(), max()

--- query/function.go
+++ query/function.go
@@ -6,6 +6,20 @@ import (
 	"github.com/yuuki/dynamond/model"
 )
 
+func minInt32(x, y int32) int32 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func maxInt32(x, y int32) int32 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 // gcd is Greatest common divisor
 func gcd(a, b int) int {
 	if b == 0 {
