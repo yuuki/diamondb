4df89e0d51f6ee1353636818e7eb4828b1f1af38
y_uuki <yuki.tsubo@gmail.com>
2016-12-30 01:51:48 +0900

    Remove assert from lib/mathutil

--- lib/mathutil/math_test.go
+++ lib/mathutil/math_test.go
@@ -2,16 +2,34 @@ package mathutil
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestGcd(t *testing.T) {
-	assert.Exactly(t, 32, Gcd(128, 32))
-	assert.Exactly(t, 3, Gcd(237, 9))
+	cases := []struct {
+		Inputs []int
+		Gcd    int
+	}{
+		{[]int{128, 32}, 32},
+		{[]int{237, 9}, 3},
+	}
+	for _, c := range cases {
+		if Gcd(c.Inputs[0], c.Inputs[1]) != c.Gcd {
+			t.Fatalf("\nInput: %v\n\nExpected: %#v", c.Inputs, c.Gcd)
+		}
+	}
 }
 
 func TestLcm(t *testing.T) {
-	assert.Exactly(t, 24, Lcm(12, 24))
-	assert.Exactly(t, 756, Lcm(27, 28))
+	cases := []struct {
+		Inputs []int
+		Lcm    int
+	}{
+		{[]int{12, 24}, 24},
+		{[]int{27, 28}, 756},
+	}
+	for _, c := range cases {
+		if Lcm(c.Inputs[0], c.Inputs[1]) != c.Lcm {
+			t.Fatalf("\nInput: %v\n\nExpected: %#v", c.Inputs, c.Lcm)
+		}
+	}
 }
