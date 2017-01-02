feb20d6d7ed10103e9ab781fa0d78149ad0b8fcf
y_uuki <yuki.tsubo@gmail.com>
2017-01-03 00:38:20 +0900

    Move and export groupNames and splitName to use in redis

---
+++ lib/storage/util.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"strings"
+)
+
+func GroupNames(names []string, count int) [][]string {
+	nameGroups := make([][]string, 0, (len(names)+count-1)/count)
+	for i, name := range names {
+		if i%count == 0 {
+			nameGroups = append(nameGroups, []string{})
+		}
+		nameGroups[len(nameGroups)-1] = append(nameGroups[len(nameGroups)-1], name)
+	}
+	return nameGroups
+}
+
+// roleA.r.{1,2,3,4}.loadavg
+func SplitName(name string) []string {
+	open := strings.IndexRune(name, '{')
+	close := strings.IndexRune(name, '}')
+	var names []string
+	if open >= 0 && close >= 0 {
+		prefix := name[0:open]
+		indices := name[open+1 : close]
+		suffix := name[close+1:]
+		for _, i := range strings.Split(indices, ",") {
+			names = append(names, prefix+i+suffix)
+		}
+	} else {
+		names = strings.Split(name, ",")
+	}
+	return names
+}
---
+++ lib/storage/util_test.go
@@ -0,0 +1,37 @@
+package storage
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestGroupNames(t *testing.T) {
+	var names []string
+	for i := 1; i <= 5; i++ {
+		names = append(names, fmt.Sprintf("server%d.loadavg5", i))
+	}
+	nameGroups := SroupNames(names, 2)
+	expected := [][]string{
+		{"server1.loadavg5", "server2.loadavg5"},
+		{"server3.loadavg5", "server4.loadavg5"},
+		{"server5.loadavg5"},
+	}
+	if !reflect.DeepEqual(nameGroups, expected) {
+		t.Fatalf("\nExpected: %+v\nActual:   %+v", expected, nameGroups)
+	}
+}
+
+func TestSplitName(t *testing.T) {
+	name := "roleA.r.{1,2,3,4}.loadavg"
+	names := SplitName(name)
+	expected := []string{
+		"roleA.r.1.loadavg",
+		"roleA.r.2.loadavg",
+		"roleA.r.3.loadavg",
+		"roleA.r.4.loadavg",
+	}
+	if !reflect.DeepEqual(names, expected) {
+		t.Fatalf("\nExpected: %+v\nActual:   %+v", expected, names)
+	}
+}
