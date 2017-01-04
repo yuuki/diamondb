9b34fcbabb725e8114ceb363aebe788c2d72949c
y_uuki <yuki.tsubo@gmail.com>
2017-01-05 00:50:07 +0900

    Add GenerateSeriesSlice for testing

--- lib/series/testing.go
+++ lib/series/testing.go
@@ -7,3 +7,26 @@ func NewFloat64PointerSlice(vals []float64) []*float64 {
 	}
 	return p
 }
+
+func GenerateSeriesSlice() SeriesSlice {
+	values1 := make([]*float64, 0, 100)
+	for i := 0; i < 100; i++ {
+		v := float64(i + 1)
+		values1 = append(values1, &v)
+	}
+	values2 := make([]*float64, 0, 100)
+	for i := 0; i < 100; i++ {
+		v := float64(i + 1)
+		values2 = append(values2, &v)
+	}
+	values3 := make([]*float64, 0, 1)
+	v := float64(1)
+	values3 = append(values3, &v)
+
+	ss := make(SeriesSlice, 3)
+	ss[0] = NewSeries("server0.loadavg5", values1, int64(0), 1)
+	ss[1] = NewSeries("server1.loadavg5", values2, int64(0), 1)
+	ss[2] = NewSeries("server2.loadavg5", values3, int64(0), 1)
+
+	return ss
+}
