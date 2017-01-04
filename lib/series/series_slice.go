465c95e5c6739a03ec2779bceea66dcc762881aa
y_uuki <yuki.tsubo@gmail.com>
2017-01-05 00:50:21 +0900

    Add Normalize()

--- lib/series/series_slice.go
+++ lib/series/series_slice.go
@@ -3,6 +3,8 @@ package series
 import (
 	"sort"
 	"strings"
+
+	"github.com/yuuki/diamondb/lib/mathutil"
 )
 
 type SeriesSlice []Series
@@ -19,4 +21,22 @@ func (ss SeriesSlice) FormatedName() string {
 	}
 	sort.Strings(names)
 	return strings.Join(names, ",")
+}
+
+func (ss SeriesSlice) Normalize() (int64, int64, int) {
+	if len(ss) < 1 {
+		return 0, 0, 0
+	}
+	var (
+		step  = ss[0].Step()
+		start = ss[0].Start()
+		end   = ss[0].End()
+	)
+	for _, s := range ss {
+		step = mathutil.Lcm(step, s.Step())
+		start = mathutil.MinInt64(start, s.Start())
+		end = mathutil.MaxInt64(end, s.End())
+	}
+	end -= (end - start) % int64(step)
+	return start, end, step
 }
--- lib/series/series_slice_test.go
+++ lib/series/series_slice_test.go
@@ -28,3 +28,42 @@ func TestFormatSeries_NotUniq(t *testing.T) {
 		t.Fatalf("\nExpected: %+v\nActual:   %+v", expected, name)
 	}
 }
+
+var testNormalizeTests = []struct {
+	ss    SeriesSlice
+	start int64
+	end   int64
+	step  int
+}{
+	{
+		SeriesSlice{}, 0, 0, 0,
+	},
+	{
+		SeriesSlice{NewSeries(
+			"server1.loadavg5",
+			NewFloat64PointerSlice([]float64{0.0, 0.0, 0.0, 0.0, 0.0}),
+			int64(1),
+			1,
+		)},
+		int64(1), int64(5), 1,
+	},
+	{
+		GenerateSeriesSlice(),
+		int64(0), int64(99), 1,
+	},
+}
+
+func TestNormalize(t *testing.T) {
+	for i, nt := range testNormalizeTests {
+		start, end, step := nt.ss.Normalize()
+		if start != nt.start {
+			t.Fatalf("\nExpected: %+v\nActual:   %+v (#%d)", nt.start, start, i)
+		}
+		if end != nt.end {
+			t.Fatalf("\nExpected: %+v\nActual:   %+v (#%d)", nt.end, end, i)
+		}
+		if step != nt.step {
+			t.Fatalf("\nExpected: %+v\nActual:   %+v (#%d)", nt.step, step, i)
+		}
+	}
+}
