d41fe15bcf91f4b476104f71ac1f5082c37ab636
y_uuki <yuki.tsubo@gmail.com>
2017-01-08 00:16:36 +0900

    Add Series.AsResp()

--- lib/series/series.go
+++ lib/series/series.go
@@ -1,5 +1,7 @@
 package series
 
+import "math"
+
 type Series interface {
 	Name() string
 	Values() []float64
@@ -9,6 +11,7 @@ type Series interface {
 	Len() int
 	SetAlias(s string)
 	Alias() string
+	AsResp() *SeriesResp
 }
 
 type series struct {
@@ -61,4 +64,38 @@ func (s *series) Alias() string {
 		return s.Name()
 	}
 	return s.alias
+}
+
+/*
+An example of json response
+{
+    "target": "server1.cpu.softirq.percentage",
+    "datapoints": [
+      [
+        0.244669050464,
+        1474725188
+      ],
+      [
+        0.236104685209,
+        1474725248
+      ],
+}
+*/
+
+type SeriesResp struct {
+	Target     string          `json:"target"`
+	DataPoints [][]interface{} `json:"datapoints"`
+}
+
+func (s *series) AsResp() *SeriesResp {
+	points := make([][]interface{}, 0, s.Len())
+	for i, v := range s.Values() {
+		timestamp := s.Start() + int64(s.Step()*i)
+		if math.IsNaN(v) {
+			points = append(points, []interface{}{nil, timestamp})
+		} else {
+			points = append(points, []interface{}{v, timestamp})
+		}
+	}
+	return &SeriesResp{Target: s.Alias(), DataPoints: points}
 }
--- lib/series/series_test.go
+++ lib/series/series_test.go
@@ -1,6 +1,9 @@
 package series
 
 import (
+	"encoding/json"
+	"fmt"
+	"math"
 	"testing"
 
 	"github.com/kylelemons/godebug/pretty"
@@ -41,5 +44,19 @@ func TestSeriesAlias(t *testing.T) {
 	s.SetAlias("func(server1.loadavg5)")
 	if s.Alias() != "func(server1.loadavg5)" {
 		t.Fatalf("\nExpected: %+v\nActual:   %+v", "func(server1.loadavg5)", s.Alias())
+	}
+}
+
+func TestSeriesAsResp(t *testing.T) {
+	s := NewSeries("server1.loadavg5", []float64{0.1, 0.2, 0.3, math.NaN(), 0.5}, 1000, 60)
+	s.SetAlias("func(server1.loadavg5)")
+	sresp := s.AsResp()
+	j, err := json.Marshal(sresp)
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	expected := "{\"target\":\"func(server1.loadavg5)\",\"datapoints\":[[0.1,1000],[0.2,1060],[0.3,1120],[null,1180],[0.5,1240]]}"
+	if fmt.Sprintf("%s", j) != expected {
+		t.Fatalf("\nExpected: %+v\nActual:   %+v", expected, j)
 	}
 }
