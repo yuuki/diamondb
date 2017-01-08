9e21b18ce1efc3e622240227dc4ea375e9565496
y_uuki <yuki.tsubo@gmail.com>
2017-01-08 21:46:15 +0900

    Fix Go style

--- lib/series/testing.go
+++ lib/series/testing.go
@@ -15,10 +15,11 @@ func GenerateSeriesSlice() SeriesSlice {
 	v := float64(1)
 	values3 = append(values3, v)
 
-	ss := make(SeriesSlice, 3)
-	ss[0] = NewSeries("server0.loadavg5", values1, int64(0), 1)
-	ss[1] = NewSeries("server1.loadavg5", values2, int64(0), 1)
-	ss[2] = NewSeries("server2.loadavg5", values3, int64(0), 1)
+	ss := SeriesSlice{
+		NewSeries("server0.loadavg5", values1, int64(0), 1),
+		NewSeries("server1.loadavg5", values2, int64(0), 1),
+		NewSeries("server2.loadavg5", values3, int64(0), 1),
+	}
 
 	return ss
 }
