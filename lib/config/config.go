1df497d057f42493911fab7251a7b5fef5e529f7
y_uuki <yuki.tsubo@gmail.com>
2017-01-09 11:37:14 +0900

    Add DIAMONDB_REDIS_ADDR DIAMONDB_REDIS_PASSWORD DIAMONDB_REDIS_DB as new parameters

--- lib/config/config.go
+++ lib/config/config.go
@@ -2,19 +2,27 @@ package config
 
 import (
 	"os"
-	// "github.com/pkg/errors"
+	"strconv"
+
+	"github.com/pkg/errors"
 )
 
 type config struct {
-	Host string
-	Port string
+	Host          string
+	Port          string
+	RedisAddr     string
+	RedisPassword string
+	RedisDB       int
 
 	Debug bool
 }
 
 const (
-	DefaultHost = "localhost"
-	DefaultPort = "8000"
+	DefaultHost          = "localhost"
+	DefaultPort          = "8000"
+	DefaultRedisAddr     = "localhost:6379"
+	DefaultRedisPassword = ""
+	DefaultRedisDB       = 0
 )
 
 // Config is set from the environment variables
@@ -28,6 +36,24 @@ func Load() error {
 	Config.Port = os.Getenv("DIAMONDB_PORT")
 	if Config.Port == "" {
 		Config.Port = DefaultPort
+	}
+	Config.RedisAddr = os.Getenv("DIAMONDB_REDIS_ADDR")
+	if Config.RedisAddr == "" {
+		Config.RedisAddr = DefaultRedisAddr
+	}
+	Config.RedisPassword = os.Getenv("DIAMONDB_REDIS_PASSWORD")
+	if Config.RedisPassword == "" {
+		Config.RedisPassword = DefaultRedisPassword
+	}
+	redisdb := os.Getenv("DIAMONDB_REDIS_DB")
+	if redisdb == "" {
+		Config.RedisDB = DefaultRedisDB
+	} else {
+		v, err := strconv.Atoi(redisdb)
+		if err != nil {
+			return errors.New("DIAMONDB_REDIS_DB must be an integer")
+		}
+		Config.RedisDB = v
 	}
 
 	if os.Getenv("DIAMONDB_DEBUG") != "" {
