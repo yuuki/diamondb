56ff76cb24f64f8106eb0df02505791eea8225b6
y_uuki <yuki.tsubo@gmail.com>
2017-01-11 22:17:45 +0900

    Make TestFormatedName table driven

--- lib/series/series_slice_test.go
+++ lib/series/series_slice_test.go
@@ -6,30 +6,38 @@ import (
 	"github.com/kylelemons/godebug/pretty"
 )
 
-func TestFormatedName_Uniq(t *testing.T) {
-	ss := SeriesSlice{
-		NewSeries("server1.cpu.system", nil, 0, 0),
-		NewSeries("server2.cpu.system", nil, 0, 0),
-		NewSeries("server3.cpu.system", nil, 0, 0),
-	}
-	name := ss.FormatedName()
-	expected := "server1.cpu.system,server2.cpu.system,server3.cpu.system"
-	if name != expected {
-		t.Fatalf("\nExpected: %+v\nActual:   %+v", expected, name)
-	}
+var testFormatedNameTests = []struct {
+	desc string
+	ss   SeriesSlice
+	name string
+}{
+	{
+		"case1: input unique series names",
+		SeriesSlice{
+			NewSeries("server1.cpu.system", nil, 0, 0),
+			NewSeries("server2.cpu.system", nil, 0, 0),
+			NewSeries("server3.cpu.system", nil, 0, 0),
+		},
+		"server1.cpu.system,server2.cpu.system,server3.cpu.system",
+	},
+	{
+		"case2: input not unique series names",
+		SeriesSlice{
+			NewSeries("server2.cpu.system", nil, 0, 0),
+			NewSeries("server1.cpu.system", nil, 0, 0),
+			NewSeries("server3.cpu.system", nil, 0, 0),
+			NewSeries("server1.cpu.system", nil, 0, 0),
+		},
+		"server1.cpu.system,server2.cpu.system,server3.cpu.system",
+	},
 }
 
-func TestFormatSeries_NotUniq(t *testing.T) {
-	ss := SeriesSlice{
-		NewSeries("server2.cpu.system", nil, 0, 0),
-		NewSeries("server1.cpu.system", nil, 0, 0),
-		NewSeries("server3.cpu.system", nil, 0, 0),
-		NewSeries("server1.cpu.system", nil, 0, 0),
-	}
-	name := ss.FormatedName()
-	expected := "server1.cpu.system,server2.cpu.system,server3.cpu.system"
-	if name != expected {
-		t.Fatalf("\nExpected: %+v\nActual:   %+v", expected, name)
+func TestFormatedName(t *testing.T) {
+	for _, tc := range testFormatedNameTests {
+		name := tc.ss.FormatedName()
+		if name != tc.name {
+			t.Fatalf("\nExpected: %+v\nActual:   %+v", tc.name, name)
+		}
 	}
 }
 
