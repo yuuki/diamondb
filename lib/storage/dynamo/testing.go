06c0384713178cdab55bb16ea83b57f7ffb04683
y_uuki <yuki.tsubo@gmail.com>
2017-01-13 23:52:34 +0900

    Add fine-grade functions for dynamodb mock

--- lib/storage/dynamo/testing.go
+++ lib/storage/dynamo/testing.go
@@ -19,6 +19,47 @@ type mockDynamoDBParam struct {
 	SeriesMap series.SeriesMap
 }
 
+func mockExpectBatchGetItem(mock *MockDynamoDBAPI, m *mockDynamoDBParam) *gomock.Call {
+	var keys []map[string]*dynamodb.AttributeValue
+	for name, _ := range m.SeriesMap {
+		keys = append(keys, map[string]*dynamodb.AttributeValue{
+			"MetricName": {S: aws.String(name)},
+			"Timestamp":  {N: aws.String(fmt.Sprintf("%d", m.ItemEpoch))},
+		})
+	}
+	items := make(map[string]*dynamodb.KeysAndAttributes)
+	items[m.TableName] = &dynamodb.KeysAndAttributes{Keys: keys}
+	params := &dynamodb.BatchGetItemInput{
+		RequestItems:           items,
+		ReturnConsumedCapacity: aws.String("NONE"),
+	}
+	return mock.EXPECT().BatchGetItem(params)
+}
+
+func mockReturnBatchGetItem(expect *gomock.Call, m *mockDynamoDBParam) *gomock.Call {
+	responses := make(map[string][]map[string]*dynamodb.AttributeValue)
+	for name, sp := range m.SeriesMap {
+		var vals [][]byte
+		for _, point := range sp.Points() {
+			buf := new(bytes.Buffer)
+			binary.Write(buf, binary.BigEndian, int64(point.Timestamp()))
+			binary.Write(buf, binary.BigEndian, math.Float64bits(point.Value()))
+			vals = append(vals, buf.Bytes())
+		}
+		attribute := map[string]*dynamodb.AttributeValue{
+			"MetricName": {S: aws.String(name)},
+			"Timestamp":  {N: aws.String(fmt.Sprintf("%d", m.ItemEpoch))},
+			"Values":     {BS: vals},
+		}
+		responses[m.TableName] = append(responses[m.TableName], attribute)
+	}
+
+	expect.Return(&dynamodb.BatchGetItemOutput{
+		Responses: responses,
+	}, nil)
+	return expect
+}
+
 func createMockDynamoDB(t *testing.T, m *mockDynamoDBParam) (*gomock.Controller, *DynamoDB) {
 	ctrl := gomock.NewController(t)
 	dmock := NewMockDynamoDBAPI(ctrl)
