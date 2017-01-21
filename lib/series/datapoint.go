df60be12c411e2e176b9e36c9578a91e47d82a4e
y_uuki <yuki.tsubo@gmail.com>
2017-01-22 01:13:53 +0900

    Add datapoint.go comment

--- lib/series/datapoint.go
+++ lib/series/datapoint.go
@@ -6,11 +6,13 @@ import (
 	"strconv"
 )
 
+// DataPoint represents a pair of metric timestamp and value.
 type DataPoint struct {
-	timestamp int64
+	timestamp int64 // UNIX Timestamp
 	value     float64
 }
 
+// NewDataPoint returns the pointer of the DataPoint object.
 func NewDataPoint(t int64, v float64) *DataPoint {
 	return &DataPoint{
 		timestamp: t,
@@ -18,33 +20,41 @@ func NewDataPoint(t int64, v float64) *DataPoint {
 	}
 }
 
+// Timestamp returns timestamp.
 func (d *DataPoint) Timestamp() int64 {
 	return d.timestamp
 }
 
+// Value returns value.
 func (d *DataPoint) Value() float64 {
 	return d.value
 }
 
+// DataPoints represents the slice of pointer of DataPoint
 type DataPoints []*DataPoint
 
+// Len returns DataPoints length.
 func (ds DataPoints) Len() int {
 	return len(ds)
 }
 
+// Swap is for Sort interface.
 func (ds DataPoints) Swap(i, j int) {
 	ds[i], ds[j] = ds[j], ds[i]
 }
 
+// Less is for Sort interface.
 func (ds DataPoints) Less(i, j int) bool {
 	return ds[i].Timestamp() < ds[j].Timestamp()
 }
 
+// Sort sorts DataPoints in ascending order of timestamps.
 func (ds DataPoints) Sort() DataPoints {
 	sort.Sort(ds)
 	return ds
 }
 
+// Deduplicate eliminates duplications of DataPoints with the same timestamp.
 func (ds DataPoints) Deduplicate() DataPoints {
 	deduplicated := make(map[string]float64, ds.Len())
 	for _, d := range ds {
