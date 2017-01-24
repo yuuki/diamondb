3f68b0747ed54953d997a487a1f450a76414d964
y_uuki <yuki.tsubo@gmail.com>
2017-01-25 01:18:50 +0900

    Enable engine to exec groupseries

--- lib/query/engine_test.go
+++ lib/query/engine_test.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/kylelemons/godebug/pretty"
+	"github.com/pkg/errors"
+
 	. "github.com/yuuki/diamondb/lib/series"
 	"github.com/yuuki/diamondb/lib/storage"
 )
@@ -60,6 +62,33 @@ func TestEvalTarget_FuncNest(t *testing.T) {
 	)
 	expected := SeriesSlice{s1}
 	if diff := pretty.Compare(seriesSlice, expected); diff != "" {
+		t.Fatalf("diff: (-actual +expected)\n%s", diff)
+	}
+}
+
+func TestEvalTarget_GroupSeries(t *testing.T) {
+	expected := SeriesSlice{
+		NewSeries("server1.loadavg5", []float64{10.0, 11.0}, 1000, 60),
+		NewSeries("server2.loadavg5", []float64{12.0, 13.0}, 1000, 60),
+	}
+	fakefetcher := &storage.FakeFetcher{
+		FakeFetchSeriesSlice: func(name string, start, end time.Time) (SeriesSlice, error) {
+			if name != "server1.loadavg5,server2.loadavg5" {
+				return nil, errors.Errorf("unexpected name: %s", name)
+			}
+			return expected, nil
+		},
+	}
+	got, err := EvalTarget(
+		fakefetcher,
+		"server{1,2}.loadavg5",
+		time.Unix(0, 0),
+		time.Unix(120, 0),
+	)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if diff := pretty.Compare(got, expected); diff != "" {
 		t.Fatalf("diff: (-actual +expected)\n%s", diff)
 	}
 }
