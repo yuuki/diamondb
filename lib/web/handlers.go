dfbe637aa0e833fafc172dc03ab56388d3555616
y_uuki <yuki.tsubo@gmail.com>
2017-02-12 16:56:53 +0900

    Return 403 if raise ArgumentError

--- lib/web/handlers.go
+++ lib/web/handlers.go
@@ -74,7 +74,7 @@ func RenderHandler(env *env.Env) http.Handler {
 		if err != nil {
 			log.Printf("%+v", err) // Print stack trace by pkg/errors
 			switch err.(type) {
-			case *query.ParserError, *query.UnsupportedFunctionError:
+			case *query.ParserError, *query.UnsupportedFunctionError, *query.ArgumentError:
 				badRequest(w, errors.Cause(err).Error())
 			default:
 				serverError(w, errors.Cause(err).Error())
